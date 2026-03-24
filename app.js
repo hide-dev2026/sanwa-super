cff044c4691384eb7d4a978d1f2452472f24bd0b
hide-dev2026 <winwin_hide@yahoo.co.jp>
2026-03-24 18:26:19 +0900

    Add files via upload
    urlBase64ToUint8Arrayファンクションを追加

--- app.js
+++ app.js
@@ -158,6 +158,21 @@ async function subscribeUser() {
   sendSubscriptionToServer(subscription);
 }
 
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 // GAS に購読情報を送信（仮）
 async function sendSubscriptionToServer(subscription) {
   await fetch("https://sanwa-push.winwin-hide.workers.dev/register", {
