caa5057fc4092f56a928d6b4f04e7a6497aaa16c
hide-dev2026 <winwin_hide@yahoo.co.jp>
2026-04-26 21:02:08 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -51,9 +51,34 @@ if (url.hostname.includes('workers.dev')) {
 // プッシュ通知の受信
 self.addEventListener("push", event => {
   const data = event.data ? event.data.json() : {};
+  const url = new URL(event.request.url);
 
   self.registration.showNotification(data.title || "新しい通知", {
     body: data.body || "内容がありません",
     icon: "/sanwa-super/sanwa_super_icon_192.png"
   });
+
+    // ★ Worker除外
+  if (url.hostname.includes('workers.dev')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  // ★ GAS除外（これ重要）
+  if (url.hostname.includes('script.google.com')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  // ★ Google Spreadsheet系も除外（超重要）
+  if (url.hostname.includes('googleusercontent.com')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+
 });
