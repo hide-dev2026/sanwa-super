61256055376faa14368fc3d4d2c8b160927340bb
hide-dev2026 <winwin_hide@yahoo.co.jp>
2026-04-26 20:46:53 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ window.addEventListener('load', () => {
 // ========================================
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/sanwa-super/service-worker.js')
       .then(() => console.log('Service Worker登録成功'))
       .catch(err => console.log('Service Worker登録失敗', err));
   });
