6ea36beedc807652e1b81e49f721b55dd24404b6
hide-dev2026 <winwin_hide@yahoo.co.jp>
2026-04-29 15:11:44 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -82,10 +82,7 @@ async function loadProducts() {
     const container = document.getElementById("product-list");
     container.innerHTML = "";
 
-    console.log(rows);
-
     rows.slice(1).forEach(cols => {
-      console.log(cols);
       const name = cols[0];
       const price = cols[1];
 
