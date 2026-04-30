95a53c0ea1b68d029b4f161b5505de34752fd450
hide-dev2026 <winwin_hide@yahoo.co.jp>
2026-04-30 16:35:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PRODUCTS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTI_Y9
 // ========================================
 // 🔔 GAS（購読保存用）
 // ========================================
-const GAS_DEPLOY_URL = "https://script.google.com/macros/s/AKfycbyY32VaDKPbGjfFredyCugv7sONYSrxjb66dABqom3LMhQ8m-7UHzsZzObES92H9R8M/exec";
+const GAS_DEPLOY_URL = "https://script.google.com/macros/s/AKfycby4XqNmsLUcGPkQif3UDAOQ6WYh8f80Q8CPoe07TYwc9B8dmp2olq9H6EGsJOAq6M6B/exec";
 
 // ========================================
 // 📥 CSV取得
